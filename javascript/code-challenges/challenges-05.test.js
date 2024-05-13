68db5ce79ffd8682f9509ab7038560577a1d7b97
Jace <sockgnome@att.net>
2024-05-13 21:56:49 +0000

    Solved challenge 3

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -38,7 +38,8 @@ Write a function named checkValues that takes in an object and a value and retur
 ------------------------------------------------------------------------------------------------ */
 
 const checkValues = (obj, value) => {
-  // Solution code here...
+  let tfBool = Object.values(obj).includes(value);
+  return tfBool;
 };
 
 /* ------------------------------------------------------------------------------------------------
