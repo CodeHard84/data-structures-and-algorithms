15677cd1f69b0162c79ce53067c139d5a4e685ca
Jace <sockgnome@att.net>
2024-05-21 12:29:49 +0000

    Removed one-liner to pass GH build test.

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -12,7 +12,8 @@ Note the space in between first and last names.
 You can assume that neither firstName nor lastName will be blank
 ------------------------------------------------------------------------------------------------ */
 const toLastNames = people => {
-  return people.map(person => `${person.firstName} ${person.lastName}`);
+  const newObj = people.map(person => `${person.firstName} ${person.lastName}`);
+  return newObj;
 };
 
 /* ------------------------------------------------------------------------------------------------
